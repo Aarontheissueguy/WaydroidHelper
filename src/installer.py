ef04506b4fe14ae06fb34b5a64339bea1b8fb1b7
Jami Kettunen <jami.kettunen@protonmail.com>
2024-03-27 23:39:41 +0200

    Provide some support for custom Android 13 image setup
    This simplifies the setup process on a Volla Phone X23 for example with
    added disclaimers until upstream Waydroid is ready for releasing.

--- src/installer.py
+++ src/installer.py
@@ -6,17 +6,43 @@ import sys
 sys.path.append('deps')
 sys.path.append('../deps')
 import pexpect
+import subprocess
+import urllib.request
+import shutil
 
 import password_type
 
 class Installer:
+    click_rootdir = os.getcwd()
+
+    def needs_custom_images(self) -> bool:
+        if os.uname().machine != "aarch64": # images only compiled for arm64
+            return False
+
+        if shutil.which("getprop") is None:
+            return False
+
+        try:
+            vndk = int(subprocess.check_output(["getprop", "ro.vndk.version"]))
+        except ValueError:
+            return False
+
+        if vndk <= 30: # only concerns Android 12, 12L & 13 atm
+            return False
+
+        try:
+            c = urllib.request.urlopen("https://ota.waydro.id/vendor/waydroid_arm64/HALIUM_13.json")
+            c.close()
+            return False
+        except urllib.error.HTTPError as e:
+            return e.code == 404
+
     def get_password_type(self):
         return password_type.get_password_type()
 
-    def install(self,password,gAPPS):
-        
+    def install(self, password, gAPPS, needsCustomImages):
         os.chdir("/home/phablet")
-        
+
         #Starting bash and getting root privileges
         print("Starting bash and getting root privileges")
         pyotherside.send('state', 'starting', False)
@@ -31,15 +57,19 @@ class Installer:
 
         #Initializing waydroid (downloading lineage)
         def download():
-            if gAPPS == True:
-                print("Initializing waydroid wit GAPPS (downloading lineage)")
+            if needsCustomImages:
+                print("Initializing waydroid with custom images (downloading lineage)")
+                pyotherside.send('state', 'dl.init.vanilla', True)
+                child.sendline(f"python3 {self.click_rootdir}/src/waydroid-custom-init")
+            elif gAPPS == True:
+                print("Initializing waydroid with GAPPS (downloading lineage)")
                 pyotherside.send('state', 'dl.init.gapps', True)
                 child.sendline("waydroid init -s GAPPS")
             else:
                 print("Initializing waydroid (downloading lineage)")
                 pyotherside.send('state', 'dl.init.vanilla', True)
                 child.sendline("waydroid init")
-        
+
         def dlstatus():
             print("Download status running")
             downloaded = []
@@ -66,12 +96,12 @@ class Installer:
                         pyotherside.send('state', 'dl.vendor', True)
                     else:
                         pyotherside.send('state', 'dl.gapps' if gAPPS == True else 'dl.vanilla', True)
-                    
+
                     progress = float(child.match.group(1))
                     target = float(child.match.group(2))
                     speed = float(child.match.group(3))
                     unit = child.match.group(4)
-                    
+
                     pyotherside.send('downloadProgress', progress, target, speed, unit)
 
                 index = child.expect(["Validating system image", pexpect.EOF, pexpect.TIMEOUT], timeout=0.5)
@@ -83,10 +113,10 @@ class Installer:
 
                 if 'system' in downloaded and 'vendor' in downloaded:
                     break
-            
+
         trd1 = threading.Thread(target=download)
         trd2 = threading.Thread(target=dlstatus)
-        
+
         trd1.start()
         trd2.start()
 
@@ -97,12 +127,12 @@ class Installer:
         child.close()
 
         pyotherside.send('state', 'complete', False)
-        
+
         return ""
     
     def uninstall(self, password, wipe=False):
         os.chdir("/home/phablet")
-        
+
         #Starting bash and getting root privileges
         print("Starting bash and getting root privileges")
         pyotherside.send('state', 'starting', False)
@@ -126,6 +156,9 @@ class Installer:
         pyotherside.send('state', 'cleanup', False)
         child.sendline("rm -rf /var/lib/waydroid/*")
         child.expect('root.*')
+        if os.path.isdir("/etc/waydroid-extra"):
+            child.sendline("rm -rf {/userdata/system-data,}/etc/waydroid-extra/*")
+            child.expect('root.*')
         child.sendline("rm -f /home/phablet/.local/share/applications/Waydroid.desktop")
         child.expect('root.*')
 
